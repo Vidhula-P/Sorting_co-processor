33080f76466a16accd34f387fd6473d216f741b0
Vidhula Pallavor <78312866+Vidhula-P@users.noreply.github.com>
2026-02-05 14:32:07 -0500

    Count sort for radix sort

--- dpi.cc
+++ dpi.cc
@@ -1,18 +1,18 @@
-// C handles Merge Sort
+// C handles Radix Sort
 #include "stdio.h"
 #include "stdlib.h"
 
 // Declare the SV functions
 extern "C" void call_sv_from_c();
 
-extern "C" int sv_sort(int *arr);
+extern "C" int count_sort(int *arr, int exp);
 
 void call_sv_from_c() {
-  int arr [4] = {6,2,4,5};
+  int arr [20] = {120,110,949,584,9,0,92,80,49,0,3,48,106,77,385,76,444,73,28,100};
   
-  sv_sort(arr);
+  count_sort(arr, 1);
   printf("C got sorted array: ");
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 20; i++)
     printf("%d ", arr[i]);
   
   printf("\n");
--- testbench.sv
+++ testbench.sv
@@ -1,24 +1,52 @@
-// TimSort is a hybrid sorting algorithm that uses the ideas of Merge Sort and Insertion Sort.
-// Verilog handles Insertion sort
+// Verilog handles Count sort
 
-module testbech;
+module testbench;
 
   // Export SV function to C
-  export "DPI-C" function sv_sort;
+  export "DPI-C" function count_sort;
 
   // cannot pass dynamic array in export function
-  function void sv_sort(inout int arr[4]);
-    int i, j, key;
+  function void count_sort(inout int arr[20],input int exp);
+    int out [20]; // will store output
+    int count[10]; // stores number of occurences of each digit
+    int i, n;
+    
+    n = 20; // size of array; 
+    		//fixed because export functions cannot have dynamic array
+    
     $display("\nSV received: arr=%0p", arr);
-    for(i=1; i<4; i++) begin
-      key = arr[i]; // current element being considered
-      j = i-1;
-      while (j>=0 && key<arr[j]) begin
-        arr[j+1] = arr[j];
-        j-=1;
-      end
-      arr[j+1] = key;
+    
+    // Store count of occurrences in count[]
+    for (int i = 0; i < n; i++) begin
+      count[(arr[i] / exp) % 10]+=1;
     end
+    
+    $display("Step 1: count = %p",count);
+    
+    // Change count[i] so that count[i] now 
+    // contains actual position of this digit
+    // in output[]
+    for (int i = 1; i < 10; i++) begin
+      count[i] += count[i - 1]; // cumulative add
+    end
+    
+    $display("Step 2: count = %p",count);
+    
+    // Build the output array
+    for (int i = n - 1; i >= 0; i--) begin
+        out[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    end
+    
+    $display("Step 3: out = %p",out);
+    
+    // Copy the output array back to arr[]
+    for (int i = 0; i < n; i++) begin
+        arr[i] = out[i];
+    end
+    
+    $display("Step 4: out = %p",out);
+    
   endfunction
 
   // Import C function
