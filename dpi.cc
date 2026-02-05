e36d808ec3df567e3718699abda65f1b209b37d8
Vidhula Pallavor <78312866+Vidhula-P@users.noreply.github.com>
2026-02-05 16:07:23 -0500

    working radix sort

--- dpi.cc
+++ dpi.cc
@@ -1,17 +1,38 @@
 // C handles Radix Sort
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-// Declare the SV functions
+int max_element (int *arr);
+
+// Helper function to return the largest element
+int max_element (int *arr) {
+  int len = sizeof(arr) / sizeof(arr[0]);
+  int res = arr[0];
+  for (int i =0; i<len; i++) {
+    if (arr[i]>res)
+      res = arr[i];
+  }
+  return (res);
+}
+
+
 extern "C" void call_sv_from_c();
 
 extern "C" int count_sort(int *arr, int exp);
 
+// Declare the SV functions
 void call_sv_from_c() {
-  int arr [20] = {120,110,949,584,9,0,92,80,49,0,3,48,106,77,385,76,444,73,28,100};
+  int arr [20] = {12660,110,949,584,9,0,92,80,49,78482,78481,3,48,77,385,76,444,73,28,100};
   
-  count_sort(arr, 1);
-  printf("C got sorted array: ");
+  //int limit = max_digits(arr);
+  int max = max_element(arr);;
+  
+  for (int i = 1; max/i!= 0; i=i*10) {
+    count_sort(arr, i);
+  }
+  
+  printf("Sorted array from SystemVerilog: ");
   for (int i = 0; i < 20; i++)
     printf("%d ", arr[i]);
   
--- testbench.sv
+++ testbench.sv
@@ -6,7 +6,7 @@ module testbench;
   export "DPI-C" function count_sort;
 
   // cannot pass dynamic array in export function
-  function void count_sort(inout int arr[20],input int exp);
+  function automatic void count_sort(inout int arr[20],input int exp);
     int out [20]; // will store output
     int count[10]; // stores number of occurences of each digit
     int i, n;
@@ -14,14 +14,10 @@ module testbench;
     n = 20; // size of array; 
     		//fixed because export functions cannot have dynamic array
     
-    $display("\nSV received: arr=%0p", arr);
-    
     // Store count of occurrences of a digit in count[digit]
     for (int i = 0; i < n; i++) begin
       count[(arr[i] / exp) % 10]+=1;
     end
-    
-    $display("Step 1: count = %p",count);
     
     // Change count[i] so that count[i] now 
     // contains actual position+1 of this digit in output[]
@@ -31,8 +27,6 @@ module testbench;
       count[i] += count[i - 1]; // cumulative add
     end
     
-    $display("Step 2: count = %p",count);
-    
     // Build the output array
     // since for one digit, count sort preserves insertion order, we start from end of array
     for (int i = n - 1; i >= 0; i--) begin
@@ -40,14 +34,10 @@ module testbench;
         count[(arr[i] / exp) % 10]--;
     end
     
-    $display("Step 3: out = %p",out);
-    
     // Copy the output array back to arr[]
     for (int i = 0; i < n; i++) begin
         arr[i] = out[i];
     end
-    
-    $display("Step 4: arr = %p",arr);
     
   endfunction
 
